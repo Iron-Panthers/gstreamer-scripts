29449ca8392573e23991aee2d8a33463740d37af
Maxim <maximumpwnguin@gmail.com>
2019-02-12 09:40:23 -0800

    add in pseyestart

--- begin-streaming-server.sh
+++
@@ -1,3 +0,0 @@
-# Creates a tmux session for the streaming server if it does not exist already. 
-
-tmux has -t vidstream || tmux new -sd vidstream "./start-streaming-server.sh"
--- start-jetson-server.bat
+++
@@ -1,2 +0,0 @@
-:: TODO make a private key for convenience
-ssh -t ubuntu@10.50.26.19 "/home/ubuntu/gstreamer-scripts/open-streaming-server.sh"
